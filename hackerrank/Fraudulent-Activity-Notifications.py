4a1a8bbf9d82241f97941cc269aff6621d2f6baf
yyuuliang <yulianl@g.clemson.edu>
2020-03-06 12:03:49 -0800

    frist commit

---
+++ hackerrank/Fraudulent-Activity-Notifications.py
@@ -0,0 +1,75 @@
+'''
+File: Fraudulent-Activity-Notifications.py
+Source: hackerrank
+Author: yyuuliang@github
+-----
+File Created: Friday, 6th March 2020 11:55:35 am
+Last Modified: Friday, 6th March 2020 11:56:04 am
+-----
+Copyright: MIT
+'''
+
+#
+# Fraudulent Activity Notifications
+# https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+#
+
+# counting sort
+
+# mp is the position we want to find in the array
+# here it is the middle one or each of the middle two
+def findmedian(ca,mp):
+    cp = mp+1
+    median=0
+    for i in range(len(ca)):
+        if ca[i]>0:
+            cp-=ca[i]
+        if cp<=0:
+            median=i
+            break
+    return median
+
+def findmm(ca,d):
+    m=0
+    if d%2 ==1:
+        m=findmedian(ca,int(d/2))
+    else:
+        m1=findmedian(ca,d/2)
+        m2=findmedian(ca,d/2-1)
+        m=(m1+m2)/2
+    return m
+
+def activityNotifications(expenditure, d):
+    # generate the first count array
+    ca =[0]*(max(expenditure)+1)
+    for n in expenditure[0:d]:
+        ca[n]+=1
+    # find median from the counting array
+    m=findmm(ca,d)
+    # compare with next
+    idx = d
+    p=0
+    idx_pop=0
+    if expenditure[idx]>=m*2:
+        p+=1
+    # then we start the rolling
+    for n in expenditure[d:]:
+        # print(ca)
+        # pop  the front item
+        ca[expenditure[idx_pop]]-=1
+        idx_pop+=1
+        # add the idx one
+        ca[n]+=1
+        # compute the median of the new ca
+        m=findmm(ca,d)
+        # compare with next
+        idx+=1
+        if idx < len(expenditure) and expenditure[idx]>=m*2:
+            p+=1
+    return p
+
+a=[2, 3, 4, 2, 3, 6, 8, 4, 5] 
+d=5
+print(a)
+p=activityNotifications(a,d)
+print(p)
---
+++ hackerrank/Merge-Sort-Counting-Inversions.py
@@ -0,0 +1,64 @@
+'''
+File: Merge-Sort-Counting-Inversions.py
+Source: hackerrank
+Author: yyuuliang@github
+-----
+File Created: Friday, 6th March 2020 11:57:20 am
+Last Modified: Friday, 6th March 2020 11:57:22 am
+-----
+Copyright: MIT
+'''
+
+#
+# Merge Sort: Counting Inversions
+# https://www.hackerrank.com/challenges/ctci-merge-sort/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+#
+
+# merge sort
+
+def countInversions(a):
+    if len(a)>1:
+        m=int(len(a)/2)
+        la=a[0:m]
+        ra=a[m:]
+        s1=countInversions(la)
+        s2=countInversions(ra)
+        # now merge left and right together
+        ll=len(la)
+        lr=len(ra)
+        li, ri,mi =0,0,0
+        swap=0
+        # swap happens here
+        if s1 is not None:
+            swap +=s1
+        if s2 is not None:
+            swap +=s2
+        while True:
+            if la[li]>ra[ri]:
+                # to move from right to the most-front left, we have to cross (ll-li) numbers
+                # whcih means (ll-li) swaps
+                swap+=(ll-li)
+                a[mi]=ra[ri]
+                ri+=1
+            else:
+                a[mi]=la[li]
+                li+=1
+            mi+=1
+            if li==ll:
+                # put the rest of R
+                for n in ra[ri:]:
+                    a[mi]=n
+                    mi+=1
+                break
+            if ri==lr:
+                # put the rest of L
+                for n in la[li:]:
+                    a[mi]=n
+                    mi+=1
+                break
+        return swap
+        
+a=[12, 11, 13, 5, 6, 7]
+# a=[2 ,1, 3, 1, 2]
+sw=countInversions(a)
+print(a,sw)
